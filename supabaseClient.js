50089e373f555cc3d6e5ed96a6b486b181608c0c
GorylevIvan <ivan.gorylev@bk.ru>
2026-07-17 17:26:04 +0300

    Change supabase file

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const SUPABASE_URL = "https://gxfttbffheesltpkzdzy.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4ZnR0YmZmaGVlc2x0cGt6ZHp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNDQwODcsImV4cCI6MjA5MzgyMDA4N30.RWxAtlJqJIAi_TADw4YFwLb9T4q_GvDLP5iJ-PNDXjY";
+export const SUPABASE_URL = "https://ffdklgcdpwdxbqyvcppc.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZGtsZ2NkcHdkeGJxeXZjcHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyOTIxNjYsImV4cCI6MjA5OTg2ODE2Nn0.F5UII7Z7p8H_Yy9fBwLGf3DmhCqkPD9NDv7hZpJ302A";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
